f6e812dcad9ff356971206dd422e93c55d3d7b2d
Andrey Kraveckiy <aok@anadeainc.com>
2018-11-16 14:38:25 +0200

    Add seed to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!([
+  {name: "Most-CITY", latitude: 48.466817, longitude: 35.050803},
+  {name: "Le Grand", latitude: 48.462553, longitude: 35.051366},
+  {name: "Museum of interesting science", latitude: 48.468769, longitude: 35.055903}
+])
